b9c6087c512f4bf4b44950aa3fe924e47482d0d2
Weiwen <gwengww@gmail.com>
2020-08-24 14:13:07 -0500

    add load credential from file and refresh

--- pysuite/auth.py
+++ pysuite/auth.py
@@ -4,6 +4,8 @@ import pickle
 import logging
 
 from google.oauth2.credentials import Credentials
+from google.auth.transport.requests import Request
+from google_auth_oauthlib.flow import InstalledAppFlow
 
 CREDENTIALS = {
     "drive":["token"],
@@ -12,10 +14,11 @@ CREDENTIALS = {
 
 
 class Authentication:
+    SCOPE = None  # overload in child class
 
-    def __init__(self, credential: Optional[Union[PosixPath, str, dict]], token: Optional[Union[PosixPath, str]]=None):
+    def __init__(self, credential: Optional[Union[PosixPath, str, dict]], token: Optional[Union[PosixPath, str]]):
+        self._token_path = Path(token)
         self._credential = self.load_credential(credential, token=token)
-        self.refresh()
 
     def load_credential(self, credential: Optional[Union[PosixPath, str, dict]],
                         token: Optional[Union[PosixPath, str]]=None):
@@ -23,7 +26,7 @@ class Authentication:
             return self._load_token(token)
 
         if credential is None:
-            return self._load_credentail_from_input()
+            return self._load_credential_from_input()
 
         if isinstance(credential, str):
             credential = Path(credential)
@@ -35,12 +38,12 @@ class Authentication:
             return self._load_credential_from_dict(credential)
 
     def _load_credential_from_file(self, file_path: PosixPath):
-        pass
+        return InstalledAppFlow.from_client_secrets_file(file_path, self.SCOPE)
 
     def _load_credential_from_dict(self, credential: dict):
         pass
 
-    def _load_credentail_from_input(self):
+    def _load_credential_from_input(self):
         pass
 
     def _load_token(self, token: Union[PosixPath, str, dict]):
@@ -53,7 +56,18 @@ class Authentication:
         return credentials
 
     def refresh(self):
-        pass
+        if not self._credential.valid:
+            if self._credential.expired and self._credential.refresh_token:
+                self._credential.refresh(Request())
+            else:
+                # need to manually authenticate once
+                self._credential.run_local_server(port=9999)
+
+        self.write_token()
+
+    def write_token(self):
+        with open(self._token_path, 'wb') as token:
+            pickle.dump(self._credential, token)
 
     def get_client(self):
         raise NotImplementedError
