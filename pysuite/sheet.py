9cb775b9967bbbbd1870f913790feef13f5b39df
Weiwen <gwengww@gmail.com>
2020-08-25 14:55:37 -0500

    implement upload and clear method and their coverage

--- pysuite/sheet.py
+++ pysuite/sheet.py
@@ -22,8 +22,21 @@ class Sheet:
         values = result.get('values', [])
         return values
 
-    def upload(self, id: str, range: str):
-        pass
+    def upload(self, values: list, id: str, range: str):
+        self.clear(id=id, range=range)
+        body = {"values": values}
+        logging.info(f"Updating sheet '{id}' range '{range}'")
+        request = self._client.values().update(spreadsheetId=id,
+                                              range=range,
+                                              valueInputOption="RAW",
+                                              body=body)
+        result = request.execute()
+        msg = f"{result.get('updatedRange')} has been updated ({result.get('updatedRows')} rows " \
+              f"and {result.get('updatedColumns')} columns)"
+        logging.info(msg)
+
+    def clear(self, id: str, range: str):
+        self._client.values().clear(spreadsheetId=id, range=range, body={}).execute()
 
     def read_sheet(self, id: str, range: str):
         pass
--- tests/test_sheet.py
+++ tests/test_sheet.py
@@ -5,6 +5,9 @@ from pysuite.sheet import Sheet
 from googleapiclient.errors import HttpError
 from tests.test_auth import sheet_client
 
+test_sheet_id = "1CNOH3o2Zz05mharkLXuwX72FpRka8-KFpIm9bEaja50"
+
+
 @pytest.fixture()
 def sheet(sheet_client):
     return Sheet(client=sheet_client.get_client())
@@ -27,7 +30,34 @@ def sheet(sheet_client):
                              ])
                          ])
 def test_download_return_correct_values(sheet, dimension, expected):
-    result = sheet.download(id="1CNOH3o2Zz05mharkLXuwX72FpRka8-KFpIm9bEaja50",
+    result = sheet.download(id=test_sheet_id,
                             range="download!A1:C",
                             dimension=dimension)
     assert result == expected
+
+
+def test_upload_and_clear_change_sheet_value_correctly(sheet):
+    range = "upload!A1:B"
+
+    values = [["a", "b"], ["c", "d"], [1, 2]]
+    sheet.upload(values=values, id=test_sheet_id, range=range)
+
+    result = sheet.download(id=test_sheet_id, range=range)
+    expected = [["a", "b"], ["c", "d"], ["1", "2"]]
+    assert result == expected
+
+    sheet.clear(id=test_sheet_id, range=range)
+
+    result_cleared = sheet.download(id=test_sheet_id, range=range)
+    assert result_cleared == []
+
+
+@pytest.fixture()
+def clear_sheet(sheet):
+    def clear():
+        sheet.clear(id=test_sheet_id, range="upload!A1:B")
+
+    clear()
+    yield
+    clear()
+
