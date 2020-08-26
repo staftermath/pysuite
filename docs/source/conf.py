fa2ad3843091557d020a7fe35e37607420492cbb
Weiwen <gwengww@gmail.com>
2020-08-26 11:07:50 -0500

    remove print of path

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -11,7 +11,6 @@
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 import os
 import sys
-print(os.path.abspath('../..'))
 sys.path.insert(0, os.path.abspath('../..'))
 
 
