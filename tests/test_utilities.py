974bc8f03340494a35f4f29cf9f0c3e62dc39bce
Weiwen <gwengww@gmail.com>
2020-10-29 21:36:52 -0500

    implement range class to handle some A1 notation

--- tests/test_utilities.py
+++ tests/test_utilities.py
@@ -2,7 +2,9 @@ import pytest
 
 from googleapiclient.errors import HttpError
 
-from pysuite.utilities import retry_on_out_of_quota, MAX_RETRY_ATTRIBUTE, SLEEP_ATTRIBUTE
+from pysuite.utilities import (
+    retry_on_out_of_quota, MAX_RETRY_ATTRIBUTE, SLEEP_ATTRIBUTE, Shape, Range, get_col_from_number, get_column_number
+)
 
 
 class TestClass:
@@ -68,3 +70,39 @@ def test_retry_on_out_of_quota_mismatch_error_raise_error_correctly():
         test_class.foo("mismatching error msg")
 
     assert len(record) == 0
+
+
+col_to_index_params = [
+    ("A", 1),
+    ("C", 3),
+    ("AB", 28),
+    ("ZY", 701),
+    ("ZYXZY", 12337701)
+]
+
+@pytest.mark.parametrize(("col", "expected"), col_to_index_params)
+def test_get_column_number_return_correct_values(col, expected):
+    result = get_column_number(col)
+    assert result == expected
+
+
+@pytest.mark.parametrize(("expected", "index"), col_to_index_params)
+def test_get_col_from_number_return_correct_value(index, expected):
+    result = get_col_from_number(index)
+    assert expected == result
+
+
+
+@pytest.mark.parametrize(("input", "expected"),
+                         [
+                             [[1, 5, 3, 8], (4, 3)],
+                             [[1, 5, 3, None], (None, 3)]
+                         ])
+def test_shape_get_shape_correctly(input, expected):
+    shape = Shape(*input)
+    assert shape.size == expected
+
+
+def test_range_get_shape_correctly():
+    range = Range("test!A1:B4")
+    assert range.shape == Shape(1, 1, 2, 4)
