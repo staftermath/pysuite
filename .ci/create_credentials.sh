920feb6961e549dbd196d5a4bdaa11e91d9bfec5
Weiwen Gu <gwengww@gmail.com>
2021-11-02 13:27:16 -0500

    ci: use large gpg encrypted credential for ci

--- .ci/create_credentials.sh
+++ .ci/create_credentials.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 CREDENTIAL_FOLDER=$GITHUB_WORKSPACE/credentials
+ENCRYPTED_CREDENTIAL_FOLDER=$GITHUB_WORKSPACE/.ci/encrypted_credential
 mkdir ${CREDENTIAL_FOLDER}
-printf '{"installed": {"client_id":"%s", "auth_uri": "https://accounts.google.com/o/oauth2/auth", "token_uri": "https://oauth2.googleapis.com/token", "client_secret": "%s", "project_id": "pysuite-test"}}' "$client_id" "$client_secret" > ${CREDENTIAL_FOLDER}/credential.json
-printf '{"token": "%s", "refresh_token": "%s"}' "$drive_token" "$drive_refresh_token"> ${CREDENTIAL_FOLDER}/drive_token.json
-printf '{"token": "%s", "refresh_token": "%s"}' "$sheets_token" "$sheets_refresh_token"> ${CREDENTIAL_FOLDER}/sheets_token.json
-printf '{"token": "%s", "refresh_token": "%s"}' "$gmail_token" "$gmail_refresh_token"> ${CREDENTIAL_FOLDER}/gmail_token.json
-printf '{"token": "%s", "refresh_token": "%s"}' "$token" "$refresh_token"> ${CREDENTIAL_FOLDER}/token.json
-printf '{"type": "service_account","project_id": "pysuite-test","private_key_id": "%s","private_key": "%s","client_email": "vision@pysuite-test.iam.gserviceaccount.com","client_id": "%s","auth_uri": "https://accounts.google.com/o/oauth2/auth","token_uri": "https://oauth2.googleapis.com/token","auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs","client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/vision%%40pysuite-test.iam.gserviceaccount.com"}' "$gc_private_key" "$gc_private_key_id" "$gc_client_id"> ${CREDENTIAL_FOLDER}/cloud_service.json
+gpg --quiet --batch --yes --decrypt --passphrase="$credential_passphrase" --output ${CREDENTIAL_FOLDER}/credential.json ${ENCRYPTED_CREDENTIAL_FOLDER}/credential.json.gpg
+gpg --quiet --batch --yes --decrypt --passphrase="$credential_passphrase" --output ${CREDENTIAL_FOLDER}/drive_token.json ${ENCRYPTED_CREDENTIAL_FOLDER}/drive_token.json.gpg
+gpg --quiet --batch --yes --decrypt --passphrase="$credential_passphrase" --output ${CREDENTIAL_FOLDER}/sheets_token.json ${ENCRYPTED_CREDENTIAL_FOLDER}/sheets_token.json.gpg
+gpg --quiet --batch --yes --decrypt --passphrase="$credential_passphrase" --output ${CREDENTIAL_FOLDER}/gmail_token.json ${ENCRYPTED_CREDENTIAL_FOLDER}/gmail_token.json.gpg
+gpg --quiet --batch --yes --decrypt --passphrase="$credential_passphrase" --output ${CREDENTIAL_FOLDER}/token.json ${ENCRYPTED_CREDENTIAL_FOLDER}/token.json.gpg
+gpg --quiet --batch --yes --decrypt --passphrase="$credential_passphrase" --output ${CREDENTIAL_FOLDER}/cloud_service.json ${ENCRYPTED_CREDENTIAL_FOLDER}/cloud_service.json.gpg
 ls ${CREDENTIAL_FOLDER}
