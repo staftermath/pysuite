98dfcd866114e699ee6cab0ae5b56f0544fe6052
Weiwen Gu <gwengww@gmail.com>
2021-11-06 12:16:32 -0500

    release: compile doc and release 0.4.0

--- pysuite/__init__.py
+++ pysuite/__init__.py
@@ -2,5 +2,7 @@ from .auth import Authentication
 from .drive import Drive
 from .sheets import Sheets
 from .gmail import GMail
+from .vision import Vision
+from .storage import Storage
 
-__version__ = "0.3.1"
+__version__ = "0.4.0"
